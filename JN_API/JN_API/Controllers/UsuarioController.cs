ac0d10f1d9a8f319bfbed1fea3427423d9f9579c
ecalvo92 <edcalvo92@hotmail.com>
2024-05-30 18:30:34 -0600

    Versión Inicial

---
+++ JN_API/JN_API/Controllers/UsuarioController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JN_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpGet]
+        [Route("ConsultarUsuarios")]
+        public IActionResult ConsultarUsuarios()
+        {
+            return Ok("todo bien");
+        }
+
+    }
+}

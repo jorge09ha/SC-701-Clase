47c01a9f2624e1b064b9a05b1591e93f91a4b82a
ecalvo92 <edcalvo92@hotmail.com>
2024-06-06 19:42:58 -0600

    Semana 3 - Parte 1

--- JN_API/JN_API/Controllers/UsuarioController.cs
+++ JN_API/JN_API/Controllers/UsuarioController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Dapper;
+using JN_API.Entities;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 
 namespace JN_API.Controllers
 {
@@ -7,12 +10,58 @@ namespace JN_API.Controllers
     [ApiController]
     public class UsuarioController : ControllerBase
     {
-        [HttpGet]
-        [Route("ConsultarUsuarios")]
-        public IActionResult ConsultarUsuarios()
+        [HttpPost]
+        [Route("RegistrarUsuario")]
+        public async Task<IActionResult> RegistrarUsuario(Usuario ent)
         {
-            return Ok("todo bien");
+            Respuesta resp = new Respuesta();
+
+            using (var context = new SqlConnection("Server=EDUARDO;Database=JUEVES_BD;Trusted_Connection=True;TrustServerCertificate=True;"))
+            {
+                var result = await context.ExecuteAsync("RegistrarUsuario", new { ent.Identificacion, ent.Nombre, ent.Correo, ent.Contrasenna }, commandType: System.Data.CommandType.StoredProcedure);
+
+                if (result > 0)
+                {
+                    resp.Codigo = 1;
+                    resp.Mensaje = "OK";
+                    resp.Contenido = true;
+                    return Ok(resp);
+                }
+                else
+                {
+                    resp.Codigo = 0;
+                    resp.Mensaje = "La información del usuario ya se encuentra registrada";
+                    resp.Contenido = false;
+                    return Ok(resp);
+                }
+            }
         }
 
+        [HttpPost]
+        [Route("IniciarSesion")]
+        public async Task<IActionResult> IniciarSesion(Usuario ent)
+        {
+            Respuesta resp = new Respuesta();
+
+            using (var context = new SqlConnection("Server=EDUARDO;Database=JUEVES_BD;Trusted_Connection=True;TrustServerCertificate=True;"))
+            {
+                var result = await context.QueryAsync<Usuario>("IniciarSesion", new { ent.Correo, ent.Contrasenna }, commandType: System.Data.CommandType.StoredProcedure);
+
+                if (result.Count() > 0)
+                {
+                    resp.Codigo = 1;
+                    resp.Mensaje = "OK";
+                    resp.Contenido = result;
+                    return Ok(resp);
+                }
+                else
+                {
+                    resp.Codigo = 0;
+                    resp.Mensaje = "La información del usuario no se encuentra registrada";
+                    resp.Contenido = false;
+                    return Ok(resp);
+                }
+            }
+        }
     }
 }
